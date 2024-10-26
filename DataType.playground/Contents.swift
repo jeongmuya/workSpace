71c45f8fa0e02b3c1a8fbd223cefd71f33e4b32e
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2024-10-26 23:34:03 +0900

    DataType 실습

---
+++ DataType.playground/Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+//데이터 타입
+
+//Bool : 참 거짓을 표현하기 위한 타입
+var isChecked: Bool = false
+isChecked = true
+
+//Int : 64비트 정수형 타입
+var temperature: Int = -100
+temperature = 100
+
+//UInt(Unsigned Interger) : 64비트 정수형 타입
+var cakes: UInt = 100
+cakes = 200
+
+//Float: 32비트 부동소수형
+var pi: Float = 3.14
+pi = 314
+
+//Double: 64비트 부동소수형
+var pi2: Double = 3.14
+pi2 = 314
+
+//Character: 한 문자를 표현하기 위한 타입
+var sampleCharacter: Character = "A"
+sampleCharacter = "B"
+
+//String: 여러문자를 표현하기 위한 타입
+var sampleString: String = "Hello, World!"
+sampleString = "Swift"
+
+//타입 추론
+let test = 100
+type(of: test)
+
+var text: String = "Hello, World!"
+text = "Swift"
+type(of: text)
+
+//Any: 모든타입을 지칭하는 키워드
+var sampleAny: Any = 100
+
+
+//nil: 없음, 존재하지 않음을 나타내는 키워드
+var sample: Int? = nil
+type(of: sample)
+
+var sample2: String? = nil
+type(of: sample2)
+
---
+++ DataType/DataType/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DataType
+//
+//  Created by YangJeongMu on 10/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
